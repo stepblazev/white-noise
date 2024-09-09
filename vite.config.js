194ef3c62d781b8e80faeab9f5aa5fe1a6c1a0a2
stepblazev <ohvatuki@gmail.com>
2024-09-09 17:51:26 +0300

    09/09/2024

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: 'src',
+    server: {
+      port: 5000,
+      open: './index.ru.html'
+    },
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: './src/index.ru.html',
+                en: './src/index.en.html',
+            }
+        }
+    }
+});
